9a874539e363e3b085e32ba65bbc16a1e2fa7783
jes1905 <jadesanche2005@gmail.com>
2021-01-18 18:59:31 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, json, jsonify
+from datetime import date
+import os
+import requests
+
+
+app = Flask(__name__, static_folder="static")
+
+json_info = ''
+movies_path = os.path.join(app.static_folder, 'data', 'movies.json')
+with open(movies_path,'r') as raw_json:
+    json_info = json.load(raw_json)
+
+
+@app.route('/api/v1/movies', methods=['GET'])
+def movies_json():
+    movies_info = os.path.join(app.static_folder, 'data', 'movies.json')
+    with open(movies_info, 'r') as json_data:
+        json_info = json.load(json_data)
+    return jsonify(json_info) 
+    
+@app.route('/api/v1/movies/clean', methods=['GET'])
+def movies_clean():
+    results = []
+    movies_info = os.path.join(app.static_folder, 'data', 'movies.json')
+    with open(movies_info, 'r') as json_data:
+        json_info = json.load(json_data)
+    for movie in json_info:
+        results.append(movie)
+
+    return render_template('movies.html', results=results)
+    
+
+
+@app.route('/api/v1/movies_raw', methods=['GET'])
+def all_movies():
+    return jsonify(json_info)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
